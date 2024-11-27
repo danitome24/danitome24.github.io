2b61854f15582344cbadc10c40e3966644c71a52
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-11-27 23:12:32 +0100

    Updated image

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Dani";
+export const SITE_TITLE = "Daniel Tomé";
 export const SITE_TAGLINE =
   "Software Developer writing on Solidity and React"; // site tagline
 export const SITE_DESCRIPTION =
